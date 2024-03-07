9a93cbeffff9803fd8842d34645fa5be0eb5f228
Maaike Seijsener <36770@ma-web.nl>
2024-03-07 11:24:31 +0100

    tempratuur

---
+++ m3prog_project/public/02/temperatuur.php
@@ -0,0 +1,20 @@
+<?php
+// Fahrenheit naar Celsius
+ $fahrenheit = 74;
+ $celsius = ($fahrenheit - 32) / 1.8;
+ echo "{$fahrenheit} graden Fahrenheit = {$celsius} graden Celsius. <br/>";
+
+// celsius naar Fahrenheit
+$celsius = 30;
+$fahrenheit = ($celsius * 1.8) + 32;
+echo "{$celsius} graden Celsius = {$fahrenheit} graden Fahrenheit. <br/>";
+
+// Fahrenheit naar Kelvin
+ $fahrenheit = 32;
+ $kelvin = ($fahrenheit -32) * 1.8 + 273.15;
+ echo "{$fahrenheit} graden Fahrenheit = {$kelvin} graden Kelvin. <br/>";
+ 
+ // celsius naar Kelvin
+$celsius = 0;
+$kelvin = ($celsius + 273.15 );
+echo "{$celsius} graden Celsius = {$kelvin} graden Kelvin. <br/>";
